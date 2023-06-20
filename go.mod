35c0850a27f7969dc05cce38b5db5ad9147ef65b
艾尼 <ainiwaerkanji@bytedance.com>
2023-06-20 14:17:47 +0800

    aaa

---
+++ go.mod
@@ -0,0 +1,5 @@
+module test
+
+go 1.19
+
+require github.com/ahmetb/go-linq/v3 v3.2.0
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	test := map[string]string{}
+	for k, v := range test {
+		fmt.Println(k, v)
+	}
+}
